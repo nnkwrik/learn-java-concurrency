224cebbc78bb19bf32b3c33219205454dbe698e8
nnkwrik <nnkwrik@gmail.com>
2018-11-04 12:42:10 +0900

    :alien: 并发容器CopyOnWriteArrayList

---
+++ src/io/github/nnkwrik/concurrentColletion/copyOnWriteArrayList/CopyOnWriteArrayListDemo.java
@@ -0,0 +1,42 @@
+package io.github.nnkwrik.concurrentColletion.copyOnWriteArrayList;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ * @author nnkwrik
+ * @date 18/11/04 12:09
+ */
+public class CopyOnWriteArrayListDemo {
+
+    public static void main(String[] args) {
+        testInsert();
+//        testRemove();
+    }
+
+    public static void testInsert(){
+        CopyOnWriteArrayList<Integer> numbers
+                = new CopyOnWriteArrayList<>(Arrays.asList(1, 2, 3, 4));
+        Iterator<Integer> preIt = numbers.iterator();
+        numbers.add(9);
+        Iterator<Integer> newIt = numbers.iterator();
+
+        preIt.forEachRemaining(System.out::print);
+        System.out.println();
+        newIt.forEachRemaining(System.out::print);
+    }
+
+    public static void testRemove(){
+        CopyOnWriteArrayList<Integer> numbers
+                = new CopyOnWriteArrayList<>(Arrays.asList(1, 2, 3, 4));
+        Iterator<Integer> it = numbers.iterator();
+        try {
+        while (it.hasNext())
+            it.remove();
+        } catch (UnsupportedOperationException e){
+            System.out.println("抛出了UnsupportedOperationException");
+        }
+
+    }
+}
